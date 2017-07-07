44ea499f349d9feb20c4749746c02a749659f9cf
viniciusrc15 <vinicius1595@gmail.com>
2017-07-07 20:19:06 -0300

    Validando com Express

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
-var rotaHome = require('./app/routes/home')(app);
-var rotaForm = require('./app/routes/form_add_noticias')(app);
 /* EXPRESS
 app.get('/', function(req, res){
 	res.send("<html><body><h1>teste</h1></body></html>");
 });
 */
+
+
 //EJS
 /*app.get('/tecnolog', function(req, res){
 	res.render("secao/tecnologia")
 });*/
 
-app.listen(3000, function() {
+/*
+Saiu com implementação do consign
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaForm = require('./app/routes/form_add_noticias')(app);
+*/
+
+
+app.listen(8080, function() {
 	console.log('Aplicação express');
 });
--- app/routes/form_add_noticias.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-	app.get('/form_inc_noticias',function(req, res){
-		res.render("admin/form_add_noticia");
-	});
-};
